2c2e4d451fa0ddf1de74eb7ac5b9340d4c239f11
WMC Kenaz <kenaz@WMCs-MacBook-Pro.local>
2019-11-13 00:13:24 +0800

    2019 redesign

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: '#works',
+  data: {
+    works: null
+  },
+
+  mounted() {
+    fetch('assets/js/data.json')
+      .then(r => r.json())
+      .then(json => {
+        this.works = json;
+        console.log(json);
+      })
+  }
+})
